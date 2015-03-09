285386b80b8b2a23c5272980cbc8b0d45db3bd50
john1king <uifantasy@gmail.com>
2015-03-09 17:11:36 +0800

    移除 Execute 类

--- lib/backto.rb
+++ lib/backto.rb
@@ -5,24 +5,19 @@ require "backto/path"
 
 module Backto
 
-  class Execute
+  module_function
 
-    def initialize(config)
-      @config = Config.create(config)
-    end
-
-    def run
-      scanner = Scanner.new(@config[:from], @config[:exclude_patterns], @config[:link_directory])
-      scanner.each do |path, is_dir, is_recursive|
-        path = Path.new(@config, path)
-        if is_dir && is_recursive
-          path.mkdirs
-        else
-          @config[:hardlink] && !is_dir ? path.hardlink : path.softlink
-        end
+  def run(config)
+    config = Config.create(config)
+    scanner = Scanner.new(config[:from], config[:exclude_patterns], config[:link_directory])
+    scanner.each do |path, is_dir, is_recursive|
+      path = Path.new(config, path)
+      if is_dir && is_recursive
+        path.mkdirs
+      else
+        config[:hardlink] && !is_dir ? path.hardlink : path.softlink
       end
     end
-
   end
 
 end
